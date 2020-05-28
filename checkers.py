14811a90750f0daa328667d69c9924eb9dc0eaa3
Tristan Shah <123.tristan.shah@gmail.com>
2020-05-28 12:31:21 -0700

    added list of position for each color

--- checkers.py
+++ checkers.py
@@ -13,15 +13,20 @@ class Peice():
 
 class Checkers():
 	def __init__(self):
+		## Array representing board
 		self.board = None
+		## List of positions of each peice
+		self.red = []
+		self.black = []
 		self.reset()
 
+	## Initialize empty board
 	def set_board(self):
-		return np.zeros((SIZE, SIZE))
+		self.board = np.zeros((SIZE, SIZE))
 
-	## places peices on board
+	## Places peices on board
 	def place_peices(self):
-		## place white
+		## place red
 		for y in range(0, 3):
 			for x in range(SIZE):
 				if (x + y)%2 == 0:
@@ -32,28 +37,55 @@ class Checkers():
 				if (x + y)%2 == 0:
 					self.board[y,x] = -1
 
+	## Generates list of tuples for each peice position of 
+	# each color
+	def get_peices_position(self):
+		self.red = []
+		self.black = []
+		for y in range(SIZE):
+			for x in range(SIZE):
+				if self.board[y, x] == 1:
+					self.red.append((y, x))
+				elif self.board[y, x] == -1:
+					self.black.append((y, x))
+				else:
+					pass
+
 	def reset(self):
-		self.board = self.set_board()
+		self.set_board()
 		self.place_peices()
+		self.get_peices_position()
 
 	## Takes int position on board
 	## Returns (y, x) coordinate
 	def int_to_coord(self, pos):
 		return int(pos/SIZE), pos%SIZE
 
+	## Takes coodinate tuple
+	## Returns int position
+	def coord_to_int(self, coord):
+		return coord[0] * SIZE + coord[1]
+
 	## Takes a peice from start_pos (int)
 	## Moves it to end_pos (int)
 	## Returns reward
-	def move(self, start_pos, end_pos):
+	def move(self, color, start_pos, end_pos):
 		start_coord = self.int_to_coord(start_pos)
-		end_pos = self.int_to_coord(end_pos)
+		end_coord = self.int_to_coord(end_pos)
 
 		peice = self.board[start_coord]
 		self.board[start_coord] = 0
+		self.board[end_coord] = peice
 
-		self.board[end_pos] = peice
+		if color == 'red':
+			idx = self.red.index(start_coord)
+			self.red[idx] = end_coord
 
-	def get_possible_moves(self):
+		if color == 'black':
+			idx = self.black.index(start_coord)
+			self.black[idx] = end_coord
+
+	def get_possible_moves(self, color):
 		raise NotImplementedError
 
 	def render(self):
@@ -70,6 +102,4 @@ class Checkers():
 
 if __name__ == "__main__":
 	env = Checkers()
-	print(env.board)
-	env.move(16, 25)
 	print(env.board)
